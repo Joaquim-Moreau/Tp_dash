ba7988c480544e6a9438fed921eec5f4b747d99e
Joaquim-Moreau <71871342+Joaquim-Moreau@users.noreply.github.com>
2020-10-16 14:18:29 +0200

    Update tuto.py

--- tuto.py
+++ tuto.py
@@ -8,32 +8,32 @@ import sqlite3
 import seaborn as sns
 
 app = dash.Dash()
-df = pd.read_csv('carData.csv')
+# df = pd.read_csv('carData.csv')
 
 # fig = px.scatter(df, x="Year", y="Selling_Price")
 # id_fig = "prix_en_fct_de_lannee"
 
-# connexion = sqlite3.connect("C:/sqlite/Cars.sq3")
-# connexion.execute('''CREATE TABLE CAR(Car_Name, Year, Selling_Price, Present_Price,
-# Kms_Driven, Fuel_Type, Seller_Type, Transmission, Owner)  ''')
+connexion = sqlite3.connect("C:/sqlite/Cars.sq3")
+connexion.execute('''CREATE TABLE CAR(Car_Name, Year, Selling_Price, Present_Price,
+Kms_Driven, Fuel_Type, Seller_Type, Transmission, Owner)  ''')
 
-# sql = "SELECT * FROM CAR"
-# df = pd.read_sql(sql, connexion)
+sql = "SELECT * FROM CAR"
+df = pd.read_sql(sql, connexion)
 
 sns.set_theme(style="ticks")
-fig = sns.catplot(data=df, x="Car_Name", y="Selling_Price")
-id_fig = "Catplot carname"
+sns.catplot(data=df, x="Car_Name", y="Selling_Price")
+#id_fig = "Catplot carname"
 
-app.layout = html.Div([
-    dcc.Graph(
-        id=id_fig,
-        figure=fig
-                )
-            ]
-        )
+#app.layout = html.Div([
+#    dcc.Graph(
+#        id=id_fig,
+#       figure=fig
+#                )
+#            ]
+#        )
 
 # Les catplots sont utiles pour tracer des graphes avec des données qui peuvent être catégorisées dans
 # des groupes distincs comme par exemple ici les categories "Diesel" et "Petrol" ou "Manuel" et "Automatic"
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
